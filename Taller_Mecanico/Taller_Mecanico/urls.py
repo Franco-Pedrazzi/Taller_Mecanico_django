92ce39e826ff79c469c560d6f16cedf88e023866
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2025-10-20 08:36:47 -0300

    orden

--- Taller_Mecanico/Taller_Mecanico/urls.py
+++ Taller_Mecanico/Taller_Mecanico/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from django.contrib import admin
-import myapp.views as views
+import Taller_Mecanico.py.views as views
 urlpatterns = [ 
     path('admin/', admin.site.urls),
     path("", views.index, name="index") ,
