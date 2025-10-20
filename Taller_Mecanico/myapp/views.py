631ce2e94a017afa3eb982c0d9553c5dd30797e1
Franco-Pedrazzi <d48457362@alumnos.itr.edu.ar>
2025-10-20 08:29:47 -0300

    requeriments

--- Taller_Mecanico/myapp/views.py
+++ Taller_Mecanico/myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse 
 def index (request): 
  
-    f = open("myapp/templates/index.html", encoding="utf-8") 
+    f = open("templates/index.html", encoding="utf-8") 
  
     response=HttpResponse (f.read()) 
     f.close() 
