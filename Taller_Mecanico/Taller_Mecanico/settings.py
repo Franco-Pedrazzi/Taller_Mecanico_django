6d5c88e0a4459d8f9e5a356285ce954b58d9d41f
frasJuanMateo <d46710280@alumnos.itr.edu.ar>
2025-10-24 16:56:01 -0300

    Se empezo a implementar la base de datos

--- Taller_Mecanico/Taller_Mecanico/settings.py
+++ Taller_Mecanico/Taller_Mecanico/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Taller_Mecanico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TallerMecanico',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
